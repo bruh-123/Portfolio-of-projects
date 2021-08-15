357804f9fc3fd7169dd9cfa84d3abba25502bd71
bruh-123 <brunomriquelme@gmail>
2021-08-14 23:43:11 -0300

    Grab pen and paper and take some time to design this first, and then figure out how in hell to DO IT.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,11 @@
-import { Flex, Text, Button, Image } from "@chakra-ui/react";
+import { Flex, Box, Text, Button, Image } from "@chakra-ui/react";
 
 export const Home = () => {
 	return (
 		<Flex h="100vh" w="100vw" bg="blue.300">
-			<Text>Hey, What's up? I am a web developer</Text>
+			<Text pos="relative" left="50px">
+				Hey, What's up? I am a web developer
+			</Text>
 		</Flex>
 	);
 };
